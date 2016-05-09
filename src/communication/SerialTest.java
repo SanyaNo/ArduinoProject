3bee57fdfcc06894f8bef923819fa38f60337d2a
SanyaNo <sanya@abv.bg>
2016-05-09 12:36:32 +0300

    Some comments removed

--- src/communication/SerialTest.java
+++ src/communication/SerialTest.java
@@ -120,18 +120,4 @@ public class SerialTest implements SerialPortEventListener {
 		return running;
 		
 	}
-
-	/*public static void main(String[] args) throws Exception {
-		SerialTest main = new SerialTest();
-		main.initialize();
-		Thread t=new Thread() {
-			public void run() {
-				//the following line will keep this app alive for 1000 seconds,
-				//waiting for events to occur and responding to them (printing incoming messages to console).
-				try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
-			}
-		};
-		t.start();
-		System.out.println("Started");
-	}*/
 }
