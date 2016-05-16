888e2f5f752d0b4d492bb727bcbd4741c040f7fd
SanyaNo <sanya@abv.bg>
2016-05-16 20:41:14 +0300

    Bullshit

--- src/graphics/Toolbar.java
+++ src/graphics/Toolbar.java
@@ -1,17 +1,21 @@
 package graphics;
 
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
 
 public class Toolbar extends JMenuBar implements ActionListener {
 	
-	private MyButton file;
+	private JMenu file;
 	private MyButton start;
 	private MyButton stop;
 	
@@ -19,15 +23,25 @@ public class Toolbar extends JMenuBar implements ActionListener {
 		
 		setBorder(BorderFactory.createEtchedBorder());
 		
-		file = new MyButton("File");
+		file = new JMenu("File");
 		start = new MyButton("Start");
 		stop = new MyButton("Stop");
 		
-		setLayout(new FlowLayout(FlowLayout.LEFT));
+		setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
 		
-		add(file);
-		add(start);
-		add(stop);
+		gc.weightx = 1;
+		gc.weighty = 1;
+		gc.gridx = 0;
+		gc.gridy = 0;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		add(file, gc);
+		gc.insets = new Insets(0, 0, 0, 5);
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		add(start, gc);
+		gc.gridx = 2;
+		add(stop, gc);
 		
 	}//end constructor
 
